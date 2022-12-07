0d4642e4c182d40da1d027a5d7d95672b1f6bb4d
Melvyn <92712460+MelvynHoo@users.noreply.github.com>
2022-12-07 14:31:35 +0800

    Pushing

---
+++ Assets/ASG2_Folder/Scripts/VegatableInteractable.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VegatableInteractable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnHover()
+    {
+        //Look through all children and store any Mesh Renderers in the meshRenderers array
+        //Get all the renderers of this object.
+        MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
+
+        // Use a foreach loop to iterate through the meshRenderers array
+        foreach (MeshRenderer renderer in meshRenderers)
+        {
+            // Enables the Emission property of the renderer's material
+            renderer.material.EnableKeyword("_EMISSION");
+        }
+    }
+
+    public void ExitHover()
+    {
+        //Look through all children and store any Mesh Renderers in the meshRenderers array
+        //Get all the renderers of this object.
+        MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
+
+        // Use a foreach loop to iterate through the meshRenderers array
+        foreach (MeshRenderer renderer in meshRenderers)
+        {
+            // Enables the Emission property of the renderer's material
+            renderer.material.DisableKeyword("_EMISSION");
+        }
+    }
+
+    public void OnSelectEnter()
+    {
+        Debug.Log("vegatable is in placed");
+    }
+}
