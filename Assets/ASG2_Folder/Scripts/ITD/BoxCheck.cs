18ee834641d274352d72adc36d87120c32258dc1
Melvyn <S10228085@connect.np.edu.sg>
2022-12-13 22:37:56 +0800

    Added Box response

--- Assets/ASG2_Folder/Scripts/ITD/BoxCheck.cs
+++ Assets/ASG2_Folder/Scripts/ITD/BoxCheck.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class BoxCheck : MonoBehaviour
 {
+
+    XRSocketInteractor socketItemOne;
     // Start is called before the first frame update
     void Start()
     {
-        
+        socketItemOne = GetComponent<XRSocketInteractor>();
     }
 
     // Update is called once per frame
-    void Update()
+    public void socketCheck()
     {
-        
+        IXRSelectInteractable objName = socketItemOne.GetOldestInteractableSelected();
+        Debug.Log(objName.transform.name + " in socket of " + transform.name);
     }
 }
