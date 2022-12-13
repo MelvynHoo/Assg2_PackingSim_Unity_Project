8ada42f4a54a335a51dda1075b69a8251acd064e
Melvyn <S10228085@connect.np.edu.sg>
2022-12-13 22:05:32 +0800

    Item One Dispense
    Create a dispenser that create a prefab of item one and spawn it on the convenyor belt

---
+++ Assets/ASG2_Folder/Scripts/ITD/DispenseItemOne.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DispenseItemOne : MonoBehaviour
+{
+
+    public Transform dispenseOrigin;
+
+    public GameObject ItemOnePrefab;
+    
+    public void ToDispenseItemOne()
+    {
+        // Instantiate a bullet using the bulletPrefab
+        //User the position of the bulletOrigin as the start position of the new bullet
+        // Use the rotation o f the gun as the start rotation of the new bullet
+        GameObject newBullet = Instantiate(ItemOnePrefab, dispenseOrigin.position, transform.rotation);
+
+        newBullet.GetComponent<Rigidbody>().AddForce(dispenseOrigin.forward * 50f);
+    }
+}
--- Assets/ASG2_Folder/Scripts/ITD/HighlightInteractable.cs
+++ Assets/ASG2_Folder/Scripts/ITD/HighlightInteractable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VegatableInteractable : MonoBehaviour
+public class HighlightInteractable : MonoBehaviour
 {
     // Start is called before the first frame update
     public void OnHover()
