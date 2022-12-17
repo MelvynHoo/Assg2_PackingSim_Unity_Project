abc120cbb0561c6d28545db6273393b4df7374ea
Melvyn <92712460+MelvynHoo@users.noreply.github.com>
2022-12-17 23:21:10 +0800

    Conveyor Script
    Also computer crashed during this period, lucky got local backup

---
+++ Assets/ASG2_Folder/Scripts/ITD/Conveyor.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    public float speed = 100;
+    public Vector3 direction;
+    public List<GameObject> onBelts;
+    //Rigidbody rBody;
+    //public float spinningSpeed = 50;
+    void Start()
+    {
+        //rBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        for(int i = 0; i < onBelts.Count ; i++)
+        {
+            onBelts[i].GetComponent<Rigidbody>().velocity = speed * direction * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onBelts.Add(collision.gameObject);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        onBelts.Remove(collision.gameObject);
+    }
+
+    /*
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 pos = rBody.position;
+        rBody.position = -transform.forward * speed * Time.deltaTime;
+        rBody.MovePosition(pos);
+        //transform.Rotate(0f, spinningSpeed * Time.deltaTime, 0f, Space.Self);
+    }
+    */
+}
--- Assets/ASG2_Folder/Scripts/ITD/DispenseItem.cs
+++ Assets/ASG2_Folder/Scripts/ITD/DispenseItem.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DispenseItemOne : MonoBehaviour
+public class DispenseItem : MonoBehaviour
 {
 
     public Transform dispenseOrigin;
 
     public GameObject ItemOnePrefab;
     
-    public void ToDispenseItemOne()
+    public void ToDispenseItem()
     {
         // Instantiate a bullet using the bulletPrefab
         //User the position of the bulletOrigin as the start position of the new bullet
         // Use the rotation o f the gun as the start rotation of the new bullet
         GameObject newBullet = Instantiate(ItemOnePrefab, dispenseOrigin.position, transform.rotation);
 
-        newBullet.GetComponent<Rigidbody>().AddForce(dispenseOrigin.forward * 50f);
+        newBullet.GetComponent<Rigidbody>().AddForce(dispenseOrigin.forward * 100f);
     }
 }
